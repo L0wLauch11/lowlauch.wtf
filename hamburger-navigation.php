e201f8ca5284fca8a9a31c11690eb18a3c2df63f
L0wLauch11 <42972587+L0wLauch11@users.noreply.github.com>
2023-03-14 14:42:29 +0100

    generic site.php generator

---
+++ hamburger-navigation.php
@@ -0,0 +1,46 @@
+<link rel="stylesheet" href="/style/hamburger-navigation.css">
+<div class="hamburger-menu">
+    <?php
+    $folderToUpper = ucwords($folder);
+
+    echo "<button class='collapsible hamburger-menu-button' onclick='openHamburger()'><img src='/img/hamburger.svg'
+            class='dark-invert'>
+            <p>Meine $folderToUpper</p>
+            </button>";
+    ?>
+
+    <div class="dropdown-content">
+
+        <?php
+        $root = $_SERVER["DOCUMENT_ROOT"];
+
+        $directories = glob("$root/$folder/*", GLOB_ONLYDIR);
+        $entries = array();
+        $yearsOrder = array();
+
+        foreach ($directories as $directory) {
+            $directoryName = basename($directory);
+
+            $meta = parse_ini_file("$directory/metadata.ini");
+            $name = $meta['name'];
+            $year = $meta['year']; // used for sorting
+        
+            $entry = "<h3><a href='/site?type=$folder&name=$directoryName'>$name<img src='/img/fancy-link.svg' class='fancy-link-icon dark-invert'></a></h3>";
+
+            $entries["$year"] = $entry;
+            array_push($yearsOrder, $year);
+        }
+
+        sort($yearsOrder, SORT_NUMERIC);
+        $sortedEntries = array_replace(array_flip($yearsOrder), $entries);
+
+        foreach($sortedEntries as $item) {
+            print $item;
+        }
+        ?>
+
+        <div style="padding-bottom: 4px;"></div>
+    </div>
+</div>
+
+<script src="/js/hamburger.js" defer></script>
---
+++ site.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php
+    $root = $_SERVER["DOCUMENT_ROOT"];
+
+    include "$root/head-data-extra.html";
+
+    // Init game page
+    if (!isset($_GET['name']))
+        return;
+
+    $codename = $_GET['name'];
+    $folder = $_GET['type'];
+
+    $directory = "$root/$folder/" . $codename;
+    if (!is_dir($directory)) {
+        $codename = "oh-no"; // The folder oh-no to exist
+        $directory = "$root/$folder/" . $codename;
+    }
+
+    $metadata = parse_ini_file("$directory/metadata.ini");
+    $description = file_get_contents("$directory/description.html");
+
+    function siteMetaPrint($metadataName)
+    {
+        global $metadata;
+        print $metadata[$metadataName];
+        return $metadata[$metadataName];
+    }
+
+    function getSiteMeta($metadataName)
+    {
+        global $metadata;
+        return $metadata[$metadataName];
+    }
+
+    ?>
+
+    <link rel="stylesheet" href="style/image-viewer.css">
+    <script src="js/image-viewer.js" defer></script>
+
+    <title>🎮
+        <?php siteMetaPrint('name'); ?>
+    </title>
+</head>
+
+<body>
+    <header>
+        <?php include "$root/navigation.html"; ?>
+    </header>
+
+    <div class="container">
+
+        <div id="myModal" class="modal">
+            <span class="close">&times;</span>
+            <img class="modal-content" id="modalImg">
+        </div>
+
+        <?php include "$root/hamburger-navigation.php"; ?>
+
+        <h1><a href=<?php siteMetaPrint('link'); ?>><?php siteMetaPrint('name'); ?><img src="/img/fancy-link.svg"
+                    class="fancy-link-icon dark-invert"></a> (<?php siteMetaPrint('year'); ?>)
+        </h1>
+
+        <div class="description-box">
+            <p>
+            <h3>Über „<?php siteMetaPrint('name'); ?>“:
+            </h3>
+            <?php print $description; ?>
+            </p>
+        </div>
+
+        <div class="screenshot-box">
+            <?php
+            function is_dir_empty($dir)
+            {
+                $handle = opendir($dir);
+                while (false !== ($entry = readdir($handle))) {
+                    if ($entry != "." && $entry != "..") {
+                        closedir($handle);
+                        return false;
+                    }
+                }
+                closedir($handle);
+                return true;
+            }
+
+            // Show all screenshots in a folder
+            $directory = "$root/$folder/" . $codename;
+            $screenshotsDir = "$directory/screenshots";
+
+            if (is_dir_empty($screenshotsDir)) {
+                return;
+            }
+
+            echo '<h2>Screenshots</h2>';
+
+            $files = array();
+            foreach (scandir("$screenshotsDir") as $file) {
+                $fileName = basename("$screenshotsDir/$file");
+                $imageSource = "$folder/" . $codename . "/screenshots/$fileName";
+
+                if ($file !== '.' && $file !== '..') {
+                    echo "<img class='img-clickable' src='$imageSource'>";
+                }
+            }
+            ?>
+        </div>
+
+        <?php
+        if (getSiteMeta('direct_download') == "")
+            return;
+
+        $direct_download = getSiteMeta('direct_download');
+        $direct_download_button_text = getSiteMeta('direct_download_button_text');
+
+        echo '<h2>Herunterladen!</h2>';
+        echo "<h3><a href=$direct_download class='button'>
+            $direct_download_button_text<img src='/img/fancy-link.svg' class='fancy-link-icon'
+            style='filter: invert(1);'>
+            </a></h3>";
+        ?>
+
+
+
+    </div>
+</body>
+
+</html>
